726819e93d21273f6d2166e6fb23cf3525b22a3a
Ilia <tereshchen@sfedu.ru>
2023-12-25 20:35:58 +0300

    Full code change!(With test)

--- TeamCity/Program.cs
+++ TeamCity/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System;
+public class Program
+{
+    static void Main()
+    {
+        Debug.Assert(IsPrime(3) == true);
+        Debug.Assert(IsPrime(6) == false);
+        Debug.Assert(IsPrime(73) == true);
+    }
+    public static bool IsPrime(int x)
+    {
+        if (x <= 1) return false;
+        if (x == 2) return true;
+        if (x % 2 == 0) return false;
+        for (int i = 3; i <= Math.Sqrt(x); i += 2) { if (x % i == 0) { return false; } }
+        return true;
+    }
+}
