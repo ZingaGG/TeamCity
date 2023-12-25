ddc745f471220deae522741bef3ae09480f23bdd
Ilia <tereshchen@sfedu.ru>
2023-12-25 21:00:21 +0300

    Script changed

--- script.ps1
+++ script.ps1
@@ -1,1 +1,5 @@
-Write-Host "Hello from PowerShell!"
+if ("%ShouldRunTests%" -eq "true") {
+    dotnet test YourProject.Tests\YourProject.Tests.csproj
+} else {
+    Write-Host "Tests are skipped as ShouldRunTests is set to false."
+}
