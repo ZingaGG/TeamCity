a8428b80d6ed707ea3e2e6f35684fb15c6f09c01
Ilia <tereshchen@sfedu.ru>
2023-12-25 21:04:06 +0300

    Tests = false

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
-if ("%ShouldRunTests%" -eq "true") {
+if ("%ShouldRunTests%" -eq "false") {
     dotnet test YourProject.Tests\YourProject.Tests.csproj
 } else {
     Write-Host "Tests are skipped as ShouldRunTests is set to false."
